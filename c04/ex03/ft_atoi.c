46ed19ad08340207477beb38176b1c4f03768758
davidrfernande <david.fernandes.2k3@gmail.com>
2023-09-13 13:36:33 +0100

    atoi

---
+++ c04/ex03/ft_atoi.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+
+int ft_atoi(char *str)
+{
+    int x;
+    int n;
+    int final;
+
+    x = 0;
+    n = 1;
+    final = 0;
+    while(str[x] == '\f' || str[x] == '\n' || str[x] == '\r' ||
+        str[x] == '\n' || str[x] == '\t' || str[x] == '\v' || str[x] == ' ')
+        x++;
+    while(str[x] == '-' || str[x] == '+')
+        if(str[x] == '-')
+        {
+            n = n * -1;
+        }
+        x++;
+    while(str[x] <= '9' && str[x] >= '0')
+    {
+        final = final * 10;
+        final = final + str[x] -48;
+        x++;
+    }   
+    return (final * n);    
+}
+
+#include <stdio.h>
+
+int     main(void)
+{
+    char str1[] = "    ---+--+1234ab567";
+
+    printf("%d", ft_atoi(str1));
+}
