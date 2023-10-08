0f610c11e0773e068b86f5bf4ad1a870d65bc363
davidrfernande <david.fernandes.2k3@gmail.com>
2023-10-08 23:57:10 +0100

    08/10

--- 42/libft/ft_memmove.c
+++ 42/libft/ft_memmove.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: davidro2 <davidro2@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 18:30:31 by davidro2          #+#    #+#             */
-/*   Updated: 2023/10/06 18:35:05 by davidro2         ###   ########.fr       */
+/*   Updated: 2023/10/08 23:53:03 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 void    *ft_memmove(void *dest, const void *src, size_t n)
 {
+    unsigned char * temp;
+    size_t  x;
     
+    x = 0;
+    temp = (unsigned char *)src;
+    if (!dest || !src)
+        return NULL;
+    while (n > 0)
+    {
+        (unsigned char *)dest[x] = temp[x];
+        x++; 
+    }
+    return (dest);
 }
