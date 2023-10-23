b54e04db38fee2398926abf55ae21d20424ca95d
davidrfernande <david.fernandes.2k3@gmail.com>
2023-10-23 02:55:23 +0100

    23/10 02

--- 42/libft/ft_lstadd_front.c
+++ 42/libft/ft_lstadd_front.c
@@ -1,25 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/22 19:03:01 by david             #+#    #+#             */
-/*   Updated: 2023/10/23 00:18:20 by david            ###   ########.fr       */
+/*   Created: 2023/10/23 01:47:32 by david             #+#    #+#             */
+/*   Updated: 2023/10/23 01:48:49 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-t_list  *ft_lstnew(void *content)
-{
-	t_list	*new;
-
-	new = (t_list *)malloc(sizeof(t_list));
-	if(!new)
-		return (NULL);
-	new->content = content;
-	new->next = NULL;
-	return (new);
-}
